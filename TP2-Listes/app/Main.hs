bd22ca9d5921b8796df87f2fbbdb47ee59fee735
Jonathan <jonathan.crete@outlook.fr>
2020-10-13 10:47:56 +0200

    [UPD] add exo2 of TP2

--- TP2-Listes/app/Main.hs
+++ TP2-Listes/app/Main.hs
@@ -10,18 +10,56 @@ permute :: Eq a => [a] -> [a] -> Bool
 permute [] [] = True
 permute [] _ = False
 -- delete x removes the first occurrence of x from its list argument.
-permute (x:xs) ys = 
-    x `elem` ys && permute xs (delete x ys)
+permute (x:xs) ys
+    -- if x is into ys I continue recursively 
+    -- with x deletion
+    | x `elem` ys = permute xs (delete x ys)
+    | otherwise = False
 
 permute' :: Ord a => [a] -> [a] -> Bool
 permute' xs ys = sort xs == sort ys
 
-
---reversal :: Int -> Int -> [a] -> [a]
-
-
 -- Prelude Data.List> permute' [1,2,4,8] [1,8,2,4]
 --True
+
+-- zip = [a] -> [b] -> [(a, b)]
+-- mapM_ -> display on multiline
+pairs :: [a] -> [(a, a)]
+pairs xs = zip xs (tail xs)
+
+pairs' :: [a] -> [(a, a)]
+pairs' xs = zip xs $ tail xs
+
+evenElts :: [a] -> [a]
+evenElts  [] = []
+evenElts [x] = [x]
+evenElts [x,_] = [x]
+evenElts (x:_:xs) = x: evenElts xs
+
+subLength :: [[a]] -> [([a], Int)]
+-- Applique la fonction (lambda) sur tous les elts de la liste
+subLength = map (\ xs -> (xs, length xs))
+
+appOnPairs :: (a ->c) -> (b -> d) -> [(a, b)] -> [(c, d)]
+appOnPairs f g ((x, y) : xys) = (f x, g y): appOnPairs f g xys
+
+appOnPairs' :: (a ->c) -> (b -> d) -> [(a, b)] -> [(c, d)]
+appOnPairs' f g  = map(\ (x,y) -> (f x, g y))
+
+--factors :: (Eq a) => [a] -> [[a]]
+--factors [] = [[]]
+--factors xs = tail (inits xs) ++ aux (tail xs)
+
+subSeqs :: (Eq a) => [a] -> [[a]]
+subSeqs [] = [[]]
+subSeqs (x: xs) = xss ++ map (x:) xss
+    where
+        xss = subSeqs xs
+
+subSeqs' :: (Eq a) => [a] -> [[a]]
+subSeqs' (x:xs) = let ss = subSeqs' xs in ss ++ map (x:) ss
+
+--reversal :: Int -> Int -> [a] -> [a]
 
 
 main :: IO ()
